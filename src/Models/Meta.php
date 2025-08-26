906f0593eb2607e961830d739dd6f6fc86acec79
alizharb <harbzali@gmail.com>
2025-08-26 19:18:30 -0400

    modified:   src/Models/Meta.php

--- src/Models/Meta.php
+++ src/Models/Meta.php
@@ -79,6 +79,22 @@ class Meta extends Model
     }
 
     /**
+     * Get supported locales in a flexible way.
+     * 
+     * @return array
+     */
+    protected static function getSupportedLocales(): array
+    {
+        $locales = config('meta.supported_locales', []);
+
+        if (array_values($locales) === $locales) {
+            return $locales;
+        }
+
+        return array_keys($locales);
+    }
+
+    /**
      * Detect the type of a given value.
      *
      * Used to determine which field should store the value and how it should
@@ -90,7 +106,7 @@ class Meta extends Model
     public static function detectType(mixed $value): string
     {
         if (is_array($value)) {
-            $locales = array_keys(config('meta.supportedLocales', []));
+            $locales = static::getSupportedLocales();
             if (!array_diff(array_keys($value), $locales)) {
                 return 'string';
             }
@@ -134,7 +150,7 @@ class Meta extends Model
      */
     public function setRealValue(mixed $value, ?string $locale = null): void
     {
-        $supportedLocales = array_keys(config('meta.supportedLocales', []));
+        $supportedLocales = static::getSupportedLocales();
 
         if (is_array($value) && !array_diff(array_keys($value), $supportedLocales)) {
             $this->type = 'string';
